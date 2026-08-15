e36dfdd1b187bf00cfdabda51b7fd6ec6afce1cf
cassey-ocampo <casseyocampoe@gmail.com>
2026-08-15 09:16:21 +0800

    Create appCassey.py

---
+++ appCassey.py
@@ -0,0 +1,10 @@
+print("Calculator");
+a = int (input("Enter the first number: "));
+b = int (input("Enter the other number: "));
+sum = int (a) + int (b);
+print(sum);
+
+c = int (input("Enter the first number: "));
+d = int (input("Enter the other number: "));
+dif = int (c) - int (d);
+print(dif);
